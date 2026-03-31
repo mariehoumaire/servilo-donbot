fd636ba7a9ca89374f096c1f48fdc221c122d683
Dimitri Merejkowsky <dimitri@dmerej.info>
2026-03-31 17:12:26 +0200

    Code jour 2

--- servilo.py
+++ servilo.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 class Réponse:
     def __init__(self, texte="", code=200, headers=None):
         self.texte = texte
@@ -8,6 +11,8 @@ class Réponse:
 def réponse_pour_route(route, verbe, headers, form):
     réponse = Réponse()
 
-    réponse.texte = "Bonjour monde"
+    chemin = Path("index.html")
+    réponse.texte = chemin.read_text()
+    réponse.headers["Content-Type"] = "text/html;charset=utf-8"
 
     return réponse
